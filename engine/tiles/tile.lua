c4829298b1cd650238836159fc545015db64affe
Phoenix Aran <jadearan.iwnl@gmail.com>
2021-02-06 15:29:03 -0500

    tile stuff++

--- engine/tiles/tile.lua
+++ engine/tiles/tile.lua
@@ -2,8 +2,21 @@ local Class = require 'lib.class'
 local lume = require 'lib.lume'
 local bit = require 'bit'
 
-local Tile = Class {
-  init = function(self, tileData)
+local Entity = require 'engine.entities.bump_box'
+local SpriteRenderer = require 'engine.components.sprite_renderer'
+local AnimatedSpriteRenderer = require 'engine.components.sprite'
+
+local Tile = Class { __includes = Entity,
+  init = function(self, tileData, posX, posY, tileId)
+    local collisionRectZRangeX, collisionRectZRangeY = tileData:getCollisionZRange()
+    local collisionRectZRange = { min = collisionRectZRangeX, max = collisionRectZRangeY }
+    Entity.init(self, tileId, true, true, tileData.collisionRect, collisionRectZRange)
+    
+    -- TODO check if it has a hurtbox
+    -- TODO make hurtbox
+    
+    
+    
     -- use flyweight pattern via tileData instance
     self.data = tileData
   end
@@ -25,8 +38,12 @@ function Tile:isUpdatable()
   return false
 end
 
+function Tile:getSprite()
+  return self.tileData:getSprite()
+end
+
 function Tile:draw()
-  
+  self.tileSprite.draw(self:getPosition())
 end
 
 function Tile:pick()
--- engine/tiles/tile_data.lua
+++ engine/tiles/tile_data.lua
@@ -4,6 +4,7 @@ local bit = require 'bit'
 local TileType = require 'engine.tiles.tile_type'
 local BitTag = require 'engine.utils.bit_tag'
 local SpriteBank = require 'engine.utils.sprite_bank'
+local TileSpriteRenderer = require 'engine.tiles.tile_sprite_renderer'
 
 -- used to validate tile types provided by data scripter
 local TileTypeInverse = lume.invert(TileType)
@@ -19,7 +20,10 @@ local TileData = Class {
     self.hitbox = false
     
     self.collisionRect = { x = 0, y = 0, w = 16, y = 16 }
+    self.collisionRectZRange = { min = 0, max = 1  }
     self.hitRect = { x = 0, y = 0, w = 16, y = 16 }
+    self.hitRectZRange = { min = 0, max = 1 }
+    
 
     self.physicsLayer = 0
     self.collidesWithLayer = 0
@@ -95,6 +99,32 @@ end
 
 function TileData:getCollisionDimensions(x, y, w, h)
   return self.collisionRect.x, self.collisionRect.y, self.collisionRect.w, self.collisionRect.h
+end
+
+function TileData:getCollisionZRange()
+  return self.collisionRectZRange.min, self.collisionRectZRange.max
+end
+
+function TileData:setCollisionZRange(min, max)
+  if type(min) == 'table' then
+    self.collisionRectZRange = min
+  else
+    self.collisionRectZRange.min = min
+    self.collisionRectZRange.max = max
+  end
+end
+
+function TileData:getHurtboxZRange()
+  return self.hurtRectZRange.min, self.hurtRectZRange.max
+end
+
+function TileData:setHurtboxZRange(min, max)
+  if type(min) == 'table' then
+    self.hurtRectZRange = min
+  else
+    self.hurtRectZRange.min = min
+    self.hurtRectZRange.max = max
+  end
 end
 
 function TileData:setCollisionDimension(x, y, w, h)
