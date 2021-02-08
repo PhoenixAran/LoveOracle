7daf7b168c9c68dca20bb583938249a157844eae
Phoenix Aran <jadearan.iwnl@gmail.com>
2021-02-07 23:57:58 -0500

    content control++

--- main.lua
+++ main.lua
@@ -1,46 +1,9 @@
 local Monocle = require 'lib.monocle'
 local gameConfig = require 'game_config'
+local ContentControl = require 'engine.control.content_control'
 local AssetManager = require 'engine.utils.asset_manager'
 local Slab = require 'lib.slab'
 
--- asset loading methods
-local function loadFonts()
-  AssetManager.loadFont('data/assets/fonts/monogram.ttf', 16)
-  AssetManager.loadFont('data/assets/fonts/dialogue.ttf', 10)
-end
-
-local function loadImages(directory)
-  local imageFiles = love.filesystem.getDirectoryItems(directory)
-  for _, file in ipairs(imageFiles) do
-    local path = directory .. '/' .. file
-    if love.filesystem.getInfo(path).type == 'directory' then
-      loadImages(path)
-    else
-      AssetManager.loadImage(path)
-    end
-  end
-end
-
-local function loadSpriteSheets(directory)
-  local spritesheetFiles = love.filesystem.getDirectoryItems(directory)
-  for _, file in ipairs(spritesheetFiles) do
-    local path = directory .. '/' .. file
-    -- assetManager combines base asset path for us so we need to manually combine path to get correct 
-    -- path for love.filesystem.getinfo
-    if love.filesystem.getInfo(path).type == 'directory' then
-      loadSpriteSheets(path)
-    else
-      AssetManager.loadSpriteSheetFile(path)
-    end
-  end
-end
-
-local function initBitTags()
-  local BitTag = require 'engine.utils.bit_tag'
-  for k, v in ipairs(gameConfig.physicsFlags) do
-    BitTag(v)
-  end
-end
 
 function love.load(arg)
   -- enable zerobrane studio debugging
@@ -48,26 +11,7 @@ function love.load(arg)
     if arg[#arg] == '-debug' then require('mobdebug').start() end
   end
   
-  initBitTags()
-  
-  loadFonts() 
-  loadImages('data/assets/images') 
-  loadSpriteSheets('data/assets/spritesheets')
-  
-  -- init palettes
-  local paletteBank = require 'engine.utils.palette_bank'
-  paletteBank.initialize('data.palettes')
-  
-  -- after we load images and spritesheet initialize the sprite bank
-  local spriteBank = require 'engine.utils.sprite_bank'
-  spriteBank.initialize('data.sprites')  
-  
-  -- initialize tilesets
-  local tilesetBank = require 'engine.utils.tileset_bank'
-  tilesetBank.initialize('data.tilesets')
-  
-  local tablePool = require 'engine.utils.table_pool'
-  tablePool.warmCache(64)
+  ContentControl.buildContent()
   
   --[[
     GLOBALS DECLARED HERE
