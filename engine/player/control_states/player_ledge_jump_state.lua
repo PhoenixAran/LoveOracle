fbe3cdc1ee14271973d8c6a3286aaa1d37e10b6d
Phoenix Aran <jadearan.iwnl@gmail.com>
2024-09-01 19:20:51 -0400

    progress on player ledge jump state

--- engine/player/control_states/player_ledge_jump_state.lua
+++ engine/player/control_states/player_ledge_jump_state.lua
@@ -1,3 +1,41 @@
 local Class = require 'lib.class'
+local PlayerState = require 'engine.player.player_state'
 
-local PlayerLedgeLeap
+local PlayerLedgeJumpState = Class { __includes = PlayerState,
+  init = function(self)
+    PlayerState.init(self)
+
+
+    self.stateParameters.canAutoRoomTransition = true
+    self.stateParameters.canStrafe = true
+
+    self.stateParameters.canWarp = false
+    self.stateParameters.canControlOnGround = false
+    self.stateParameters.canControlInAir = false
+    self.stateParameters.canUseWeapons = false
+    self.stateParameters.canReleaseSword = false
+    self.stateParameters.canUseWeapons = false
+  end
+}
+
+function PlayerLedgeJumpState:getType()
+  return 'player_ledge_jump_state'
+end
+
+function PlayerLedgeJumpState:onBegin(previousState)
+  -- TODO temporarily disable solid collisions
+end
+
+function PlayerLedgeJumpState:onEnd(newState)
+  -- TODO reenable collisions
+end
+
+function PlayerLedgeJumpState:onEnterRoom()
+  -- TODO
+end
+
+function PlayerLedgeJumpState:update(dt)
+  
+end
+
+return PlayerLedgeJumpState
