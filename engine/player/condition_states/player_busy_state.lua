f1a78f5a73a47c7a4650054a441d9d2218eab9d8
Phoenix Aran <jadearan.iwnl@gmail.com>
2026-03-25 02:12:53 -0400

    more tick math fixes

--- engine/player/condition_states/player_busy_state.lua
+++ engine/player/condition_states/player_busy_state.lua
@@ -12,7 +12,7 @@ local PlayerBusyState = Class { __includes = PlayerState,
   init = function(self, duration, animation)
     if duration == nil then duration = 0 end
     PlayerState.init(self)
-    self.timer = 1
+    self.timer = 0
     self.duration = duration
     self.timedActions = { }
     self.animation = animation
@@ -31,7 +31,8 @@ end
 ---@param time integer
 ---@param func function
 function PlayerBusyState:addTimedAction(time, func)
-  self.timedActions[time] = func
+  -- add + 1 for lua arrays
+  self.timedActions[time + 1] = func
 end
 
 ---@param func function
@@ -42,29 +43,26 @@ end
 -- override playerstate methods
 ---@param previousState PlayerState?
 function PlayerBusyState:onBegin(previousState)
-    self.canJump = false
-    self.canWarp = false
-    self.canLedgeJump = false
-    self.canControlOnGround = false
-    self.canControlInAir = false
-    self.canPush = false
-    self.canUseWeapons = false
-    self.canRoomTransition = false
-    self.canStrafe = false
+  self.canJump = false
+  self.canWarp = false
+  self.canLedgeJump = false
+  self.canControlOnGround = false
+  self.canControlInAir = false
+  self.canPush = false
+  self.canUseWeapons = false
+  self.canRoomTransition = false
+  self.canStrafe = false
 
-    self.timer = 1
-    --[[
-    not sure if i should invoke the first timed action here
-    or just let it naturally happen in the update method
-    if self.timedActions[1] then
-      self.timedActions[1](self)
-    end
-    ]]--
+  self.timer = 0
+
+  if self.timedActions[1] then
+    self.timedActions[1](self)
+  end
 end
 
 ---@param newState PlayerState?
 function PlayerBusyState:onEnd(newState)
-  self.timer = 1
+  self.timer = 0
   self.duration = 0
   lume.clear(self.timedActions)
   if self.endAction ~= nil then
@@ -74,10 +72,10 @@ end
 
 function PlayerBusyState:update()
   PlayerState.update(self)
-  if self.timedActions[self.timer] then
-    self.timedActions[self.timer](self)
-  end
   self.timer = self.timer + 1
+  if self.timedActions[self.timer + 1] then
+    self.timedActions[self.timer + 1](self)
+  end
   if self.duration <= self.timer then
     self:endState()
   end
