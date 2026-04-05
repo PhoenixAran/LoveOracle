a1c9870a8aafbba2d4bf4281b45c07f9c28c9e07
Phoenix Aran <jadearan.iwnl@gmail.com>
2026-04-05 01:05:48 -0400

    make entity tracker class

---
+++ engine/entities/entity_tracker.lua
@@ -0,0 +1,69 @@
+local Class = require 'lib.class'
+local lume = require 'lib.lume'
+local vector = require 'engine.math.vector'
+local SignalObject = require 'engine.signal_object'
+
+---@class EntityTracker : SignalObject
+---@field entities Entity[]
+---@field maxCount integer
+local EntityTracker = Class {
+  init = function(self, maxCount)
+    -- Initialization code here
+    SignalObject.init(self)
+    self.entities = { }
+    self.maxCount = maxCount or math.huge
+  end
+}
+
+---@param entity Entity
+---@return boolean
+function EntityTracker:addEntity(entity)
+  if entity.destroyed then
+    return false
+  end
+  if lume.count(self.entities) >= self.maxCount then
+    return false
+  end
+
+  lume.push(self.entities, entity)
+  entity:connect('entity_destroyed', self, 'onEntityDestroyed')
+  return true
+end
+
+---@param entity Entity
+function EntityTracker:remove(entity)
+  entity:disconnect('entity_destroyed', self)
+  lume.remove(self.entities, entity)
+end
+
+function EntityTracker:clear()
+  for _, entity in ipairs(self.entities) do
+    entity:disconnect('entity_destroyed', self)
+  end
+  lume.clear(self.entities)
+end
+
+---@param entity Entity
+function EntityTracker:onEntityDestroyed(entity)
+  entity:disconnect('entity_destroyed', self)
+  lume.remove(self.entities, entity)
+end
+
+function EntityTracker:isMaxedOut()
+  return lume.count(self.entities) <= self.maxCount
+end
+
+function EntityTracker:isEmpty()
+  return lume.count(self.entities) == 0
+end
+
+function EntityTracker:isAvailable()
+  return not self:isMaxedOut()
+end
+
+function EntityTracker:release()
+  SignalObject.release(self)
+  self:clear()
+end
+
+return EntityTracker
--- engine/items/weapons/item_boomerang.lua
+++ engine/items/weapons/item_boomerang.lua
@@ -1,13 +1,17 @@
 local Class = require 'lib.class'
 local lume = require 'lib.lume'
 local vector = require 'engine.math.vector'
+local ItemWeapon = require 'engine.items.weapons.item_weapon'
+local CollisionTag = require 'engine.enums.collision_tag'
+local Direction4 = require 'engine.enums.direction4'
+local EntityDebugDrawFlags = require('engine.enums.flags.entity_debug_draw_flags').enumMap
 
 -- TODO implmement
 
----@class Item
-local ItemBoomerang = Class {
+---@class Item : ItemWeapon
+local ItemBoomerang = Class { __includes = ItemWeapon,
   init = function(self, args)
-    -- Initialization code here
+    ItemWeapon.init(self, args)
   end
 }
 
